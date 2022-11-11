e346a17875ea4d4a33eddeab23a43d9275022802
Elvis Omoro <iamomoroit@gmail.com>
2022-11-11 13:57:28 +0300

    all charcters displayed

--- src/cards/CharacterCard.jsx
+++ src/cards/CharacterCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class CharacterCard extends Component {
   render() {
     return (
-      <div>
+      <div classsNmae="flex border-2 border-black">
         {/* <p>{this.props.obj.id}</p> */}
         <img
           src={this.props.obj.imageUrl}
@@ -12,19 +12,17 @@ class CharacterCard extends Component {
           loading="lazy"
           className="w-[200px] h-[200px] rounded-full"
         />
-        <div className="flex-wrap items-center">
-          <button className="ml-[22px] p-4 w-48 text-white bg-[#000000] text-[14px] rounded-[20px] capitalize">
-            {this.props.obj.title}
-          </button>
-          <button className="ml-[22px] p-4 w-48 text-white bg-[#D4AF37] text-xl rounded-[20px] capitalize">
+        <div className="m-[21px] text-[#000] font-bold mt-[11px] pb-[10px] capitalize bg-[]">
+          {this.props.obj.fullName}
+        </div>
+        <div className="grid space-y-10 flex-wrap items-center">
+           <button className="ml-[22px] p-4 w-48 text-white bg-[#D4AF37] text-xl rounded-[20px] capitalize">
             {this.props.obj.family}
           </button>
-        </div>
-        <div
-          style={{ fontSize: { lg: '24px', xs: '20px' } }}
-          className="m-[21px] text-[#000] font-bold mt-[11px] pb-[10px] capitalize"
-        >
-          {this.props.obj.Fullname}
+          <button className="ml-[22px] p-4 w-48 text-white bg-[#000000] text rounded-[20px] capitalize">
+            {this.props.obj.title}
+          </button>
+         
         </div>
       </div>
     );
